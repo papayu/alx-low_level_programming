c5e5f7117124eaf29f9ef8a9f9f19ad56af61108
root <papayu4u@gmail.com>
2022-12-10 22:41:01 -0800

    debug Hello Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 
-char program[] = ""Programming is like building a multilingual puzzle";
+char program[] = ""Programming is like building a multilingual puzzle"";
 
 
 puts(program);
