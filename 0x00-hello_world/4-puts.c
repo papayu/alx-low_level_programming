5f6a4908be44f050cf70a25e2b498c4b4d413a95
root <papayu4u@gmail.com>
2022-12-10 22:01:03 -0800

    Hello Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+
+/*
+ *working with puts on my first C programming with Alx.
+ *
+ * */
+
+
+#include <stdio.h>
+
+int main()
+{
+
+char program[] = "Programming is like building a multilingual puzzle";
+
+
+puts(program);
+
+
+return(0);
+}
