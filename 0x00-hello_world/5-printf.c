8218801513c120e87dda8b0805af18d45c52559c
root <papayu4u@gmail.com>
2022-12-11 15:50:14 -0800

    debug Hello Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- *main - using the printf
+ * main - using the printf
  *
- *return: Always 0
+ * Return: Always 0
  *
  */
  
  
- int main (void)
+ int main(void)
 {
  
  printf ("with proper grammar, but the outcome is a piece of art,\n");
