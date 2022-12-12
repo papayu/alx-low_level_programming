13fbcc19b09510f33bc1f90168648c21bf80727e
root <papayu4u@gmail.com>
2022-12-11 16:20:51 -0800

    debug Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,6 @@
  * main - using the printf
  *
  * Return: Always 0
- *
  */
  
  
@@ -14,6 +13,6 @@ int main(void)
 printf ("with proper grammar, but the outcome is a piece of art,\n");
 
 
-return (0);  
- 
+return (0);
+
  }
