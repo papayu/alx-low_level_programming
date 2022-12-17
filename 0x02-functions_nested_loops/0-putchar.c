f0a6cbe6ccb168d5fcc260c3f411faf4af47f09b
root <papayu4u@gmail.com>
2022-12-16 21:33:42 -0800

    more fuctions n Nestwd loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints our _putchar
+ *
+ * Return: Always 0 if successful
+ *
+ */
+
+int main(void)
+{
+
+
+printf ("/_putchar"
+
+
+return (0);	
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Working with more fuctions and nested loops
